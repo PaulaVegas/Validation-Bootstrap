86aebcde727e687c24e3a905e2dc584ff6a270f7
Paula <mv.paula@gmail.com>
2025-04-08 12:22:45 +0200

    workind db and user page

--- main.js
+++ main.js
@@ -1,54 +1,3 @@
-// //TODO:
-// //**Funcionalidad**:
-
-// //    - Guarda la información recogida de cada uno de los usuarios en `localStorage`.
-// //    - Implementa validación que obligue a rellenar todos los campos.
-// //    - Implementa una validación para el correo.
-// //    - Implementa una validación que comprueba que la contraseña 1 es la misma que la contraseña 2.
-// //    - Implementa una validación de contraseña.
-// document.addEventListener('DOMContentLoaded', () => {
-// // Guarda la información recogida de cada uno de los usuarios en `localStorage`
-// const formData = document.getElementById("formData");
-// const userContainer = document.getElementById("userContainer");
-
-// if(formData) {
-// const userName = document.getElementById("userName");
-// const userEmail = document.getElementById("userEmail");
-// const userPassword1 = document.getElementById("userPassword1");
-// const userPassword2 = document.getElementById("userPassword2");
-
-// //Función para guardar los datos en localStorage al enviar el formulario
-// formData.addEventListener("submit", function onSubmit(event) {
-//   event.preventDefault();
-
-//   if (userPassword1.value !== userPassword2.value) {
-//     alert("Las contraseñas no coinciden.");
-//     return;
-//   }
-
-//   localStorage.setItem(
-//     "user",
-//     JSON.stringify({
-//       name: userName.value,
-//       email: userEmail.value,
-//     })
-//   );
-
-//   // Redirigir a user.html
-//   // window.location.href = "user.html";
-//   const userText = document.createElement("h1");
-//   document.body.appendChild(userText);
-//   const userInfo= document.createElement("h3");
-//   userInfo.textContent = `Nombre: ${userName.value}, Email: ${userEmail.value}`;
-//   userList.appendChild(userInfo);
-//   carousel.innerHTML = `
-//       <h1>Bienvenido</h1>
-//       <p><strong>Nombre:</strong> ${userFromStorage.name}</p>
-//       <p><strong>Email:</strong> ${userFromStorage.email}</p>
-//     `;
-// });
-// }
-// });
 
 const formData = document.getElementById("formData");
 const userName = document.getElementById("userName");
@@ -105,18 +54,9 @@ function saveUserData() {
   localStorage.setItem("users", JSON.stringify(usersArray));
   localStorage.setItem("user", JSON.stringify({ name, email }));
 }
+
 function displayUserData() {
-  window.location.href = "user.html";
-  const carousel = document.getElementById("carousel");
-  const userFromStorage = JSON.parse(localStorage.getItem("user"));
-  if (userFromStorage) {
-    
-    carousel.innerHTML = `
-      <h1>Bienvenido</h1>
-      <p><strong>Nombre:</strong> ${userFromStorage.name}</p>
-      <p><strong>Email:</strong> ${userFromStorage.email}</p>
-    `;
-}
+   window.location.href = "user.html";
 }
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -127,4 +67,6 @@ function handleFormSubmit(event) {
 }
 formData.addEventListener("submit", handleFormSubmit);
 
+
+// Password validation example:
 //aA1234567
